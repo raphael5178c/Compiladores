8b31f918f1cdefba577c2d5236b9c5ed9ae63c4b
guilherme.juncklaus <guilherme.juncklaus@wmw.com.br>
2018-04-16 20:16:40 -0300

    adicionado arquivo gals, e codigos gerados

---
+++ resource/gals/codigo/Constants.java
@@ -0,0 +1,51 @@
+public interface Constants extends ScannerConstants, ParserConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_TOKEN_2 = 2; //","
+    int t_TOKEN_3 = 3; //";"
+    int t_TOKEN_4 = 4; //"."
+    int t_TOKEN_5 = 5; //":"
+    int t_TOKEN_6 = 6; //":="
+    int t_TOKEN_7 = 7; //"("
+    int t_TOKEN_8 = 8; //")"
+    int t_TOKEN_9 = 9; //"*"
+    int t_TOKEN_10 = 10; //"/"
+    int t_TOKEN_11 = 11; //"-"
+    int t_TOKEN_12 = 12; //"+"
+    int t_TOKEN_13 = 13; //"<"
+    int t_TOKEN_14 = 14; //">"
+    int t_TOKEN_15 = 15; //"<="
+    int t_TOKEN_16 = 16; //">="
+    int t_TOKEN_17 = 17; //"<>"
+    int t_TOKEN_18 = 18; //"="
+    int t_identificador = 19;
+    int t_numeral = 20;
+    int t_literal = 21;
+    int t_and = 22;
+    int t_begin = 23;
+    int t_call = 24;
+    int t_case = 25;
+    int t_const = 26;
+    int t_do = 27;
+    int t_else = 28;
+    int t_end = 29;
+    int t_for = 30;
+    int t_if = 31;
+    int t_integer = 32;
+    int t_not = 33;
+    int t_of = 34;
+    int t_or = 35;
+    int t_procedure = 36;
+    int t_program = 37;
+    int t_readln = 38;
+    int t_repeat = 39;
+    int t_then = 40;
+    int t_to = 41;
+    int t_until = 42;
+    int t_var = 43;
+    int t_while = 44;
+    int t_writeln = 45;
+
+}
